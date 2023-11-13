4169fbf50167301a1125dd5501c9bc99dc408d17
Hyuk0510 <hyouk970510@gmail.com>
2023-11-13 09:43:45 +0900

    Initial Commit

---
+++ SeSACSwiftUI/SeSACSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSwiftUIApp.swift
+//  SeSACSwiftUI
+//
+//  Created by 선상혁 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
